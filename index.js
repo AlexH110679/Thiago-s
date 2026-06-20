dbf25c3511d3c0468047156f1287027b380edc3e
Admin <admin@thiagos.com>
2026-06-19 21:01:44 -0500

    Fix production crash by injecting correct Supabase polyfills in index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
